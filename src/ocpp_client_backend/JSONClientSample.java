4cf9f0a3b2bd5b74c0177aed28d9de7d426023c1
Marco Villalilla <37328224+m-villalilla@users.noreply.github.com>
2018-04-11 20:12:48 +0200

    Build current version of the github project, still not working

--- src/ocpp_client_backend/JSONClientSample.java
+++ src/ocpp_client_backend/JSONClientSample.java
@@ -1,9 +1,16 @@
 package ocpp_client_backend;
 
-import eu.chargetime.ocpp.ClientEvents;
+/*import eu.chargetime.ocpp.ClientEvents;
 import eu.chargetime.ocpp.JSONClient;
 import eu.chargetime.ocpp.feature.profile.ClientCoreEventHandler;
 import eu.chargetime.ocpp.feature.profile.ClientCoreProfile;
+import eu.chargetime.ocpp.model.core.*;*/
+
+import eu.chargetime.ocpp.IClientAPI;
+import eu.chargetime.ocpp.JSONClient;
+import eu.chargetime.ocpp.feature.profile.ClientCoreEventHandler;
+import eu.chargetime.ocpp.feature.profile.ClientCoreProfile;
+import eu.chargetime.ocpp.model.Request;
 import eu.chargetime.ocpp.model.core.*;
 
 
@@ -35,7 +42,7 @@ import eu.chargetime.ocpp.model.core.*;
  */
 
 public class JSONClientSample {	
-    private JSONClient client;
+    /*private JSONClient client;
     private ClientCoreProfile core;
 
     public void connect() {
@@ -124,7 +131,7 @@ public class JSONClientSample {
                 return null; // returning null means unsupported feature
             }
         });
-        client = new JSONClient(core, "TestPoint01");
+        client = new JSONClient(core);
         client.connect("ws://test-ocpp.ddns.net:8080/steve/websocket/CentralSystemService/TestPoint01", new ClientEvents() {
         	@Override
         	public void connectionOpened() {
@@ -148,6 +155,112 @@ public class JSONClientSample {
 
         // Client returns a promise which will be filled once it receives a confirmation.
         //client.send(request).whenComplete((s, ex) -> System.out.println(s));
+    }
+
+    public void disconnect() {
+        client.disconnect();
+    }
+	*/
+	
+	private IClientAPI client;
+    private ClientCoreProfile core;
+
+    public void connect() throws Exception {
+
+        // The core profile is mandatory
+        core = new ClientCoreProfile(new ClientCoreEventHandler() {
+            @Override
+            public ChangeAvailabilityConfirmation handleChangeAvailabilityRequest(ChangeAvailabilityRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return new ChangeAvailabilityConfirmation(AvailabilityStatus.Accepted);
+            }
+
+            @Override
+            public GetConfigurationConfirmation handleGetConfigurationRequest(GetConfigurationRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public ChangeConfigurationConfirmation handleChangeConfigurationRequest(ChangeConfigurationRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public ClearCacheConfirmation handleClearCacheRequest(ClearCacheRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public DataTransferConfirmation handleDataTransferRequest(DataTransferRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public RemoteStartTransactionConfirmation handleRemoteStartTransactionRequest(RemoteStartTransactionRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public RemoteStopTransactionConfirmation handleRemoteStopTransactionRequest(RemoteStopTransactionRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public ResetConfirmation handleResetRequest(ResetRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+
+            @Override
+            public UnlockConnectorConfirmation handleUnlockConnectorRequest(UnlockConnectorRequest request) {
+
+                System.out.println(request);
+                // ... handle event
+
+                return null; // returning null means unsupported feature
+            }
+        });
+        client = new JSONClient(core);
+        client.connect("ws://test-ocpp.ddns.net:8080/steve/websocket/CentralSystemService/TestPoint01", null);
+    }
+
+    public void sendBootNotification() throws Exception {
+
+        // Use the feature profile to help create event
+        Request request = core.createBootNotificationRequest("some vendor", "some model");
+
+        // Client returns a promise which will be filled once it receives a confirmation.
+        client.send(request).whenComplete((s, ex) -> System.out.println(s));
     }
 
     public void disconnect() {
