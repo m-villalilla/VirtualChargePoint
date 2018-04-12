133c62a59634c43f6142babd9a2b2ea8757b4da5
Marco Villalilla <37328224+m-villalilla@users.noreply.github.com>
2018-04-12 23:27:36 +0200

    Small fixes

--- src/ocpp_client_backend/launchClientBackend.java
+++ src/ocpp_client_backend/launchClientBackend.java
@@ -4,18 +4,18 @@ import ocpp_client_backend.JSONClientSamplev0_5;
 
 public class launchClientBackend {
 
-	//Global configuration
-	static String serverURL = "test-ocpp.ddns.net:8080/steve/websocket/CentralSystemService/";
-	static String clientName = "TestPoint";
-	static String CPVendor = "TestVendor";
-	static String CPModel = "TestModel";
-	static String testID = "1234567890";
-	
 	public static void main(String[] args){
+		//Configuration
+		String serverURL = "test-ocpp.ddns.net:8080/steve/websocket/CentralSystemService/";
+		String clientName = "TestPoint";
+		String CPVendor = "TestVendor";
+		String CPModel = "TestModel";
+		String testID = "1234567890";
+		
 		JSONClientSamplev0_5 client = new JSONClientSamplev0_5();
 		
 		try {
-		client.connect(serverURL, clientName);
+			client.connect(serverURL, clientName);
 		} catch (Exception e) {
 			System.out.println("Error while trying to connect to the server.");
 			e.printStackTrace();
@@ -34,6 +34,7 @@ public class launchClientBackend {
 			System.out.println("Error while trying to authorize an ID");
 			e.printStackTrace();
 		}
+		
 	}
 
 }
