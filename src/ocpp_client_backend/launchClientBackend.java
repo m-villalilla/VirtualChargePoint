aae6bc5bff68a567c1717e3629fbde088b443868
Marco Villalilla <37328224+m-villalilla@users.noreply.github.com>
2018-05-28 17:43:32 +0200

    Implemented switchcase so we dont need to comment stuff out all the time

--- src/ocpp_client_backend/launchClientBackend.java
+++ src/ocpp_client_backend/launchClientBackend.java
@@ -25,69 +25,79 @@ public class launchClientBackend {
 		String CPModel 	  		= config.getProperty("CPModel");
 		String authorizationID	= config.getProperty("authorizationID.00");			//Use authorizationID.00 as example
 		
-		String[] cps = new String[40];
-		for (int i = 0; i < 40; i++) {
-			cps[i] = config.getProperty("ChargeBoxID." + (i+10));
-		}
+		String whatToTest = "STRESSTEST"; //STRESSTEST or SINGLECLIENT or WS
 		
-		OCPPServerStressTest.startTest(serverURL, cps, authorizationID);
-		
-		Chargepoint_stable client = new Chargepoint_stable(ChargeBoxID, CPVendor, CPModel, true, false);
-			
-		try {
-			client.connect(serverURL);
-			System.out.println("Client connected.");
-		} catch (Exception e) {
-			System.out.println("Error while trying to connect to the server.");
-			e.printStackTrace();
-		}
-		
-		try {
-			client.sendBootNotification();
-		} catch (Exception e) {
-			System.out.println("Error while trying to sent a boot notification");
-			e.printStackTrace();
-		}
-		
-		try {
-			client.sendAuthorizeRequest(authorizationID);
-		} catch (Exception e) {
-			System.out.println("Error while trying to authorize an ID");
-			e.printStackTrace();
-		}
-		
-		try {
-			client.checkTransactionSupport(authorizationID);
-		} catch (Exception e) {
-			System.out.println("Error while trying to start a transaction");
-			e.printStackTrace();
-		}
-		
-		try {
-			Thread.sleep(2000);	// Give the server time to respond to ongoing requests
-			client.disconnect();
-			System.out.println("Client disconnected.");
-		} catch (InterruptedException e) {
-			System.out.println("Error while trying to disconnect");
-			e.printStackTrace();
-		}
-		
-        try {
-            final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("ws://" + serverURL + "CP3211"));
-            clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
-                public void handleMessage(String message) {
-                    System.out.println(message);
-                }
-            });
-            clientEndPoint.sendMessage("{'message': null}");
+		switch (whatToTest) {
+			case "STRESSTEST":
+				String[] cps = new String[40];
+				for (int i = 0; i < 40; i++) {
+					cps[i] = config.getProperty("ChargeBoxID." + (i+10));
+				}
+				OCPPServerStressTest.startTest(serverURL, cps, authorizationID);
+				break;
+			case "SINGLECLIENT":
+				Chargepoint_stable client = new Chargepoint_stable(ChargeBoxID, CPVendor, CPModel, true, false);
+				
+				try {
+					client.connect(serverURL);
+					System.out.println("Client connected.");
+				} catch (Exception e) {
+					System.out.println("Error while trying to connect to the server.");
+					e.printStackTrace();
+				}
+				
+				try {
+					client.sendBootNotification();
+				} catch (Exception e) {
+					System.out.println("Error while trying to sent a boot notification");
+					e.printStackTrace();
+				}
+				
+				try {
+					client.sendAuthorizeRequest(authorizationID);
+				} catch (Exception e) {
+					System.out.println("Error while trying to authorize an ID");
+					e.printStackTrace();
+				}
+				
+				try {
+					client.checkTransactionSupport(authorizationID);
+				} catch (Exception e) {
+					System.out.println("Error while trying to start a transaction");
+					e.printStackTrace();
+				}
+				
+				try {
+					Thread.sleep(2000);	// Give the server time to respond to ongoing requests
+					client.disconnect();
+					System.out.println("Client disconnected.");
+				} catch (InterruptedException e) {
+					System.out.println("Error while trying to disconnect");
+					e.printStackTrace();
+				}
+				break;
+			case "WS":
+		        try {
+		            final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("ws://" + serverURL + "CP3211"));
+		            clientEndPoint.addMessageHandler(new WebsocketClientEndpoint.MessageHandler() {
+		                public void handleMessage(String message) {
+		                    System.out.println(message);
+		                }
+		            });
+		            clientEndPoint.sendMessage("{'message': null}");
 
-            // Wait 5 seconds for messages from websocket
-            Thread.sleep(5000);
-        } catch (InterruptedException ex) {
-            System.out.println("InterruptedException exception: " + ex.getMessage());
-        } catch (URISyntaxException ex) {
-            System.out.println("URISyntaxException exception: " + ex.getMessage());
-        }
+		            // Wait 5 seconds for messages from websocket
+		            Thread.sleep(5000);
+		        } catch (InterruptedException ex) {
+		            System.out.println("InterruptedException exception: " + ex.getMessage());
+		        } catch (URISyntaxException ex) {
+		            System.out.println("URISyntaxException exception: " + ex.getMessage());
+		        }
+				break;
+			default:
+				System.out.println("Please specify what you want to do.");
+				break;
+		}
 	}
 	
 	public static Properties getConfig() {
